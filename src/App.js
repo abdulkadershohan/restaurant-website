3ab30d662172919f8649c894e9e06ea82f6928f0
Abdul Kader <abdulkadirshohan@gmail.com>
2021-09-14 14:36:26 +0600

    motion added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './App.css'
+import Navbar from "./Components/Navbar";
+import { css } from "@emotion/core";
+import {PropagateLoader} from "react-spinners";
+
 
 function App() {
+  const [loading,setLoading]=useState()
+  const override=css`
+  display: block;
+    border-color: red;
+    margin-top: 20%;
+  `
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },5000);
+  }, [])
+
   return (
     <div className="App">
+      {
+        loading ? <PropagateLoader color="#3d2514" Loading={loading} css={override} size={40}/>
+            :
+            <>
+              >
+              <Navbar/>
+            </>
+
+      }
+
+
 
     </div>
   );
 }
-
 export default App;
